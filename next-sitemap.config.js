051c3d752e52cb91a64b2c5643c77d756b4f94f0
Shivam Ramraika <shivamramraika01@gmail.com>
2025-05-14 22:32:36 +0530

    Robots added

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://shivam-ramraika.vercel.app/',
-  generateRobotsTxt: true,
-}
+  siteUrl: 'https://shivam-ramraika.vercel.app',
+  generateRobotsTxt: true, // <- this auto-generates the correct robots.txt
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+  generateIndexSitemap: true,
+};
